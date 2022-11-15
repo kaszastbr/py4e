68f388e4d82579c845d2aceda17076849e189916
Tibor Kaszas <kaszas.tbr@gmail.com>
2022-11-15 20:32:35 +0100

    minor visual improvement

--- ch_2/floor_convers.py
+++ ch_2/floor_convers.py
@@ -1,3 +1,3 @@
-european_floor = input('What is the european floor? ')      # Prompt input, stores EU floor value
+european_floor = input('What is the european floor?\n')      # Prompt input, stores EU floor value
 us_floor = int(european_floor) + 1                          # US floor turned into int
 print('The floor in the United States would be', us_floor)  # Returns US floor value
