7fa18ae1c7aebcbb9f973ba1945410f103afcefb
Tibor Kaszas <kaszas.tbr@gmail.com>
2022-11-15 20:41:02 +0100

    Exercise 3, added rounding to 2 digits

--- ch_2/pay.py
+++ ch_2/pay.py
@@ -1,4 +1,5 @@
 rate = input('Hourly rate, please\n')
 hours = input('How many horus have you worked for?\n')
 payment = float(rate) * float(hours)
+payment = round(payment,2)
 print('Your gross pay will be', payment)
