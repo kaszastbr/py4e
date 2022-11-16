2ef22e9b9f9528e08dae20eb4895a53595fafc55
Tibor Kaszas <kaszas.tbr@gmail.com>
2022-11-16 22:12:10 +0100

    trying out tabs vs spaces for indentation

--- ch_2/temp.py
+++ ch_2/temp.py
@@ -1,3 +1,9 @@
 celsius = float(input('Please tell me the temperature in celsius!\n'))
 fahrenheit = round((celsius * 9 / 5) + 32)
 print('The temperature in fahrenheit is:', fahrenheit)
+
+x = 6
+
+if x > 5:
+    print('asd')
+    print('meginasd')
