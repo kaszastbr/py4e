79360f60328adfe920477ab1b64f68f0977f4fd5
Tibor Kaszas <kaszas.tbr@gmail.com>
2023-01-03 17:40:46 +0100

    ch3 exercise 2 solved

--- ch_3/new_pay.py
+++ ch_3/new_pay.py
@@ -1,8 +1,11 @@
-rate = float(input('Hourly rate, please\n'))
-hours = float(input('How many hours have you worked for?\n'))
-if hours <= 40:
-    payment = rate * hours
-else:
-    payment = (rate * 40) + (1.5 * rate * (hours - 40))
+try:
+    rate = float(input('Hourly rate, please\n'))
+    hours = float(input('How many hours have you worked for?\n'))
+    if hours <= 40:
+        payment = rate * hours
+    else:
+        payment = (rate * 40) + (1.5 * rate * (hours - 40))
+    print('Your gross pay will be', payment)
+except:
+    print("Please give me a number senpai")
 # payment = round(rate * hours,2)
-print('Your gross pay will be', payment)
