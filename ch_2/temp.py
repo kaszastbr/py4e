c408360a236043540b148bae85f3835611f773ec
Tibor Kaszás <kaszas.tbr@gmail.com>
2024-02-12 19:43:21 +0100

    additional changes

--- ch_2/temp.py
+++ ch_2/temp.py
@@ -1,9 +1,3 @@
 celsius = float(input('Please tell me the temperature in celsius!\n'))
 fahrenheit = round((celsius * 9 / 5) + 32)
 print('The temperature in fahrenheit is:', fahrenheit)
-
-x = 6
-
-if x > 5:
-    print('asd')
-    print('meginasd')
